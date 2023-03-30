5eb71c14eab72a8d70c53c20b46136994382dc75
SamuEls <samuelcontato213@gmail.com>
2023-03-30 17:12:09 -0300

    Commit inicial

---
+++ src/main/java/com/project/cine/cinebilheteria/CineBilheteriaApplication.java
@@ -0,0 +1,13 @@
+package com.project.cine.cinebilheteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBilheteriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CineBilheteriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/cine/cinebilheteria/CineBilheteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cine.cinebilheteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBilheteriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
