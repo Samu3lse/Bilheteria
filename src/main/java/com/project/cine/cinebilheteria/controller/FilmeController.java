5d05ba53c4e3c4150ba496f0121b25482cf5d0d5
SamuEls <samuelcontato213@gmail.com>
2023-03-30 17:56:11 -0300

    CiaÃr§ão conexão service

--- src/main/java/com/project/cine/cinebilheteria/controller/FilmeController.java
+++ src/main/java/com/project/cine/cinebilheteria/controller/FilmeController.java
@@ -1,32 +1,41 @@
 package com.project.cine.cinebilheteria.controller;
 
+import com.project.cine.cinebilheteria.service.FilmeService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/filmes")
 @RestController
-
+@RequiredArgsConstructor
 
 public class FilmeController {
 
+    FilmeService filmeService;
+
     @GetMapping
     public void getAll(){
-
+    filmeService.getAll();
     }
+
     @GetMapping("/{id}")
     public void getById(@PathVariable Long id){
-
+    filmeService.getById(id);
     }
+
     @PostMapping
     public void save(){
-
+    filmeService.save();
     }
+
     @PutMapping("/{id}")
-    public void update(){
-
+    public void update(@PathVariable Long id){
+    filmeService.update(id);
     }
-    @DeleteMapping("/{id}")
-    public void delete(){
 
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+    filmeService.delete(id);
 
     }
 }
---
+++ src/main/java/com/project/cine/cinebilheteria/service/FilmeService.java
@@ -0,0 +1,24 @@
+package com.project.cine.cinebilheteria.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class FilmeService {
+
+   public void getAll(){
+
+   }
+   public void getById(Long id){
+
+   }
+   public void save(){
+
+   }
+   public void update(Long id){
+
+   }
+   public void delete(Long id){
+
+   }
+}
